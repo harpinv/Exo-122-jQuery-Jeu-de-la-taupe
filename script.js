d8faa4523dd2d720aa9298c7846259107122fac1
Nux007 <007.nux@gmail.com>
2021-01-10 14:20:27 +0100

    Exo 122.

---
+++ script.js
@@ -0,0 +1,3 @@
+const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
