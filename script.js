a41df692203d0f4edf023dbf9ee90e6011b3d33d
vince <vincent.harpin@orange.fr>
2023-02-04 14:03:03 +0100

    exercice 122

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole');
+const scoreBoard = $(".score");
+const moles = $(".mole");
+
+$("#startGame").click(function () {
+    let decompte = 20;
+    let startGame = setInterval(function () {
+        if(decompte > 0) {
+            let alea = Math.floor((Math.random() * 800) + 200);
+            let interval = setInterval(function () {
+                if(alea === 0) {
+                    let valeur = Math.floor(Math.random() * 6);
+                    console.log(holes[valeur]);
+                    holes.addClass('up');
+
+                    let compte = 0;
+                    moles.click(function () {
+                        holes.removeClass("up");
+                        compte++;
+                        scoreBoard.text(compte);
+                    })
+                    clearInterval(interval);
+                    alea = Math.floor((Math.random() * 800) + 200);
+                } else {
+                    alea--;
+                    console.log(alea);
+                }
+            }, 1)
+
+            decompte--;
+            console.log(decompte);
+        } else {
+            clearInterval(startGame);
+        }
+    }, 500);
+})
+
